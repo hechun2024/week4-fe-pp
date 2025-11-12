f422d996803eed9c6f2fec7e63ed79ee747a7803
Twe He Gam Aung <Gam.TweHe@metropolia.fi>
2025-11-12 14:37:10 +0200

    Finished Iteration2/Twe He Gam Aung

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,22 @@
-import { services } from '../data'
-import Title from './Title'
-import Service from './Service'
+import { useState } from 'react';
+import { services } from '../data';
+import Title from './Title';
+import Service from './Service';
 
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services);
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+        {servicesData.map((service) => {
+          return <Service {...service} key={service.id} />;
         })}
       </div>
     </section>
-  )
-}
-export default Services
+  );
+};
+
+export default Services;
