0212ea4b0ab9f7082f3157ea16ada56f3725f465
hechun2024 <Chun.He@metropolia.fi>
2025-11-12 14:53:43 +0200

    week4-pp-iteration3-he chun

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,15 +1,22 @@
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
+import { useState } from "react";
 
 const Tours = () => {
+  const [toursData, setToursData] = useState(tours);
+
+  const removeTour = (id) => {
+    const newTours = toursData.filter((tour) => tour.id !== id);
+    setToursData(newTours);
+  };
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
         })}
       </div>
     </section>
